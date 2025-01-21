cda0bad33883fd335fce3dd352322f2e29ff5454
Denis Clics <denisclics@gmail.com>
2025-01-20 23:28:01 -0500

    Printing Output Index.html and .js files added

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Declare the variables
+let a = 3;
+let b = 5;
+let c;
+
+// Create the output string using backticks and new lines for better readability
+let num = `
+a + b: ${a + b}
+a - b: ${a - b}
+a * b: ${a * b}
+a / b: ${a / b}
+a % b: ${a % b}
+a += b: ${a += b}
+a -= b: ${a -= b}
+a *= b: ${a *= b}
+a /= b: ${a /= b}
+a %= b: ${a %= b}
+a == b: ${a == b}
+a != b: ${a != b}
+a > b: ${a > b}
+a < b: ${a < b}
+!a && !c: ${!a && !c}
+!a || !c: ${!a || !c}
+`;
+
+// Print the output in the browser's console
+console.log(num);
+
+// Declare the variables
+let first_name = "YourFirstName";
+let last_name = "YourLastName";
+let email = "your-email@mail.com";
+
+// Create the expression
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+// Print output in the browser's console
+console.log(output);
